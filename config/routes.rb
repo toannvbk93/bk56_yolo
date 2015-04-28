8a73d362ba60ce78cadbca58d64725c1e9e02eb9
lakazai <nguyenhuyhoang2404@gmail.com>
2015-04-28 10:56:34 +0700

    refs #29819 ScaffoldでAnswerのモデルとビューとコントロールを作る

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+  resources :answers
+  end 
   devise_for :users
   root :to =>'questions#index'
   # The priority is based upon order of creation: first created -> highest priority.
